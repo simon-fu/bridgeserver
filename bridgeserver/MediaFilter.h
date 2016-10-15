e5c46a4ed47acefac28630b87d99dc9f4c728ae3
simon-fu <simonok@139.com>
2016-10-15 16:10:44 +0800

    change video resend from string to uint8*

--- bridgeserver/MediaFilter.h
+++ bridgeserver/MediaFilter.h
@@ -9,6 +9,7 @@
 #include "util.h"
 #include "AppServer.h"
 #include "ini.hpp"
+#include "xcutil.h"
 
 #pragma pack(1)
 struct rtp_hdr
@@ -30,7 +31,7 @@ struct rtp_hdr
 };
 #pragma pack()
 
-/// µ÷½ÚÀÏÏµÍ³·¢ËÍµÄÒôÆµÊ±´Á
+/// ÂµËœÎ©â„Â¿Å“Å“ÂµÃ•â‰¥âˆ‘Â¢Ã€Ã•ÂµÆ’â€œÃ™âˆ†ÂµÂ Â±Â¥Â¡
 class AudioRtpFilter
 {
 public:
@@ -71,14 +72,14 @@ public:
 
 private:
 	// rtp_hdr			lastRtpHeader_;
-	// unsigned int	hop_;			/**Ã¿¸ö°üµÄĞÄÌø¼ä¸ô*/
-	// unsigned int	startTime_;		/**rtpÊ±´Á¿ªÊ¼Ê±¼ä*/
+	// unsigned int	hop_;			/**âˆšÃ¸âˆË†âˆÂ¸ÂµÆ’â€“Æ’ÃƒÂ¯Âºâ€°âˆÃ™*/
+	// unsigned int	startTime_;		/**rtpÂ Â±Â¥Â¡Ã¸â„¢Â ÂºÂ Â±Âºâ€°*/
 
 	// bool			needModTs_;
 };
 
 
-/// µ÷½Úwebrtc·¢¹ıÀ´µÄÊÓÆµ°ü
+/// ÂµËœÎ©â„webrtcâˆ‘Â¢Ï€ËÂ¿Â¥ÂµÆ’Â â€âˆ†ÂµâˆÂ¸
 class VideoRtpFilter
 {
 public:
@@ -104,16 +105,30 @@ public:
 	}
 };
 
-#define MAX_CACHE_RTP_PKT_COUNT 1024
+#define MAX_CACHE_RTP_PKT_COUNT 256
+#define MAX_RTP_BUFFER_SIZE     1600
 
-typedef std::function<int(const char* data, int dataLen)> UdpSendFunctor;
+typedef std::function<int(const uint8_t* data, int dataLen)> UdpSendFunctor;
 class VideoResendService {
 public:
-	VideoResendService(): rtpPackages_(MAX_CACHE_RTP_PKT_COUNT) {
+	VideoResendService() {
+        rtpPackets_ = new uint8_t *[MAX_CACHE_RTP_PKT_COUNT];
+        for(int i = 0; i < MAX_CACHE_RTP_PKT_COUNT; i++){
+            rtpPackets_[i] = new uint8_t[MAX_RTP_BUFFER_SIZE];
+            memset(rtpPackets_[i], 0, MAX_RTP_BUFFER_SIZE);
+        }
 		supportResend_ = g_app.iniFile().top()("GLOBAL")["xmpp_resend_video"] != "false";
 		LOG_INFO("Receive video enable: " << supportResend_ );
 //		printf("Receive video enable: %d\n", supportResend_);
 	}
+    virtual ~VideoResendService(){
+        if(rtpPackets_){
+            for(int i = 0; i < MAX_CACHE_RTP_PKT_COUNT; i++){
+                delete[] rtpPackets_[i];
+            }
+            delete[] rtpPackets_;
+        }
+    }
 
 	void setUdpSendFunctor(const UdpSendFunctor& send) {
 		send_ = send;
@@ -125,10 +140,14 @@ public:
 			LOG_WARN("Invalid rtp package!");
 			return;
 		}
-
-		LOG_DEBUG("[resend] cache data with seq: " << ntohs(*(uint16_t *)&(rtpData[2])));
-		rtpPackages_[ntohs(*(uint16_t *)&(rtpData[2])) % MAX_CACHE_RTP_PKT_COUNT].assign(rtpData, dataLen);
+        
+        uint16_t storeSeq = be_get_u16(&rtpData[2]);
+		LOG_DEBUG("[resend] cache data with seq: " << storeSeq);
+        uint8_t * buf = rtpPackets_[storeSeq % MAX_CACHE_RTP_PKT_COUNT];
+        be_set_u32(dataLen, &buf[0]);
+        memcpy(&buf[4], rtpData, dataLen);
 	}
+    
 	bool handle(const char* rtpData, int dataLen) {
 		if ((rtpData[0] & 0xff) != 0xfb) {			
 			return false;
@@ -137,37 +156,41 @@ public:
 		if (!supportResend_)
 			return true;		
 
-		uint16_t startSeq = ntohs(*((uint16_t *)&(rtpData[1])));		
-		uint16_t endSeq = ntohs(*((uint16_t *)&(rtpData[3])));
-		if (startSeq > endSeq) {
+//		uint16_t startSeq = ntohs(*((uint16_t *)&(rtpData[1])));
+//		uint16_t endSeq = ntohs(*((uint16_t *)&(rtpData[3])));
+        
+        uint16_t startSeq =  be_get_u16(&rtpData[1]);
+        uint16_t endSeq = be_get_u16(&rtpData[3]);
+        
+        
+        if (startSeq > endSeq) {
 			LOG_WARN("Receive video [resend] invalid require with start seqNo: " << startSeq << ", end seqNo: " << endSeq);
 			return true;
 		}
 
-		LOG_INFO("Receive video [resend] require with start seqNo: " << startSeq << ", end seqNo: " << endSeq);
-				
-		assert(rtpPackages_.size() == MAX_CACHE_RTP_PKT_COUNT);
+		LOG_DEBUG("Receive video [resend] require with start seqNo: " << startSeq << ", end seqNo: " << endSeq);
+
 
 		for (uint16_t seq = startSeq; seq <= endSeq; seq++)
 		{
-			std::string& packet = rtpPackages_[seq % MAX_CACHE_RTP_PKT_COUNT];
-			assert(packet.size() >= 12);
-
-			if (ntohs(*(uint16_t *)&(packet[2])) == seq)
-			{
-				LOG_DEBUG("[resend] video rtp packet seq: " << seq);
-				send_(packet.data(), packet.size());
-			}
-			else {
-				LOG_DEBUG("Rtp [resend] cache have not packet with seq: " << seq);
-			}
+            uint8_t * buf = rtpPackets_[seq % MAX_CACHE_RTP_PKT_COUNT];
+            uint32_t rtpLen = be_get_u32(&buf[0]);
+            uint8_t * rtpPkt = buf+4;
+            
+            uint16_t storeSeq = be_get_u16(&rtpPkt[2]);
+            if(rtpLen >= 12 && storeSeq == seq){
+                LOG_DEBUG("[resend] video rtp packet seq: " << seq);
+                send_(rtpPkt, rtpLen);
+            }else{
+                LOG_DEBUG("Rtp [resend] cache have not packet with seq: " << seq);
+            }
 		}
 
 		return true;
 	}
 private:
 	UdpSendFunctor				send_;
-	std::vector<std::string>	rtpPackages_;
+    uint8_t **                  rtpPackets_;
 
 	bool						supportResend_;
 };
